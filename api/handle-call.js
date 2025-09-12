ed404fa4af183c3fca77989915c93b2bf52923be
Matts369 <150201162+Matts369@users.noreply.github.com>
2025-09-12 11:15:38 -0700

    Update handle-call.js

--- api/handle-call.js
+++ api/handle-call.js
@@ -1,1 +1,10 @@
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
+export default function handler(request, response) {
+  const twiml = new VoiceResponse();
+  twiml.say('Hello from my Vercel function!');
+
+  response.setHeader('Content-Type', 'text/xml');
+  response.status(200).send(twiml.toString());
+}
 
